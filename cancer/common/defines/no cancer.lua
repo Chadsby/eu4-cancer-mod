96c6fb4d44ce1d2208c0f1180cdd45aa69a981f8
Chadsby <jkearnsschool711@gmail.com>
2024-12-14 21:03:58 -0700

    Add files via upload

---
+++ cancer/common/defines/no cancer.lua
@@ -0,0 +1,20 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+NDefines.NMilitary.PEACE_COALITION_FACTOR = 0
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10
+NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 5
+NDefines.NDiplomacy.AE_HRE_INTERNAL = 0
+NDefines.NCountry.OVERSEAS_CLIENT_STATES = 1
+NDefines.NDiplomacy.GOOD_RELATIONS = 50
+NDefines.NDiplomacy.GREAT_RELATIONS = 100
+NDefines.NGame.DAYS_BEHIND_PAUSE = 36500
+NDefines.NGame.DAYS_BEHIND_LOWER_SPEED = 73000
+NDefines.NGame.END_DATE = "1914.6.28"
+NDefines.NCountry.MAXIMUM_CONDOTTIERI = 50
+NDefines.NCountry.POLICY_COST = 0
+NDefines.NCountry.BASE_POSSIBLE_POLICIES = 6
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 18
+NDefines.NDiplomacy.PEACE_COST_CHANGE_HRE_RELIGION = 30
+NDefines.NCountry.MAX_EXTRA_PERSONALITIES = 4
+NDefines.NCountry.YEARS_PER_EXTRA_PERSONALITY = 1
---
+++ cancer/descriptor.mod
@@ -0,0 +1,7 @@
+version="1.30.*.*"
+tags={
+	"Fixes"
+}
+name="cancer"
+supported_version="1.37.*"
+remote_file_id="2126377398"
